78b86dbe61df4e72a760de7c809027d2b64dbf2d
MahmoudKhalid <mahmoud0122549@gmail.com>
2024-02-21 20:25:26 +0200

    chore: edit semester 2 button 4th year

--- app/year/page.tsx
+++ app/year/page.tsx
@@ -7,7 +7,7 @@ const Year = () => {
   return (
     <div className="year mt-36 mx-8">
       <button
-        className={`py-4 px-6 text-2xl font-semibold text-center bg-[#427D9D] text-white transition-all duration-300   ${
+        className={`py-4 w-60 text-2xl font-semibold text-center bg-[#164863] text-white transition-all duration-300   ${
           isOpen ? "rounded-tr-2xl rounded-tl-2xl" : "rounded-none"
         }`}
         onClick={() => setIsOpen(!isOpen)}
@@ -17,7 +17,7 @@ const Year = () => {
       <ul
         className={`${
           isOpen ? "h-auto max-h-[40rem]" : "max-h-0"
-        } flex flex-col items-start duration-300 overflow-hidden justify-start  bg-[#164863] w-52 transition-all z-20 shadow-2xl`}
+        } flex flex-col items-start duration-300 overflow-hidden justify-start  bg-[#427D9D] w-60 transition-all z-20 `}
       >
         <li className="text-xl text-white border-b-2 border-white  block w-full hover:pl-4 cursor-pointer transition-all duration-300">
           <Link className="py-4 block h-full px-4 w-full " href="/tables">

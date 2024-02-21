7af128e621ab9592792a95d1fd687d83dfd3eb00
MahmoudKhalid <mahmoud0122549@gmail.com>
2024-02-21 20:34:46 +0200

    chore: edit position of semester menu

--- app/year/page.tsx
+++ app/year/page.tsx
@@ -5,7 +5,7 @@ import React, { useState } from "react";
 const Year = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="year mt-36 mx-8">
+    <div className="year mt-36 mx-8 z-50">
       <button
         className={`py-4 w-60 text-2xl font-semibold text-center bg-[#164863] text-white transition-all duration-300   ${
           isOpen ? "rounded-tr-2xl rounded-tl-2xl" : "rounded-none"
@@ -17,29 +17,29 @@ const Year = () => {
       <ul
         className={`${
           isOpen ? "h-auto max-h-[40rem]" : "max-h-0"
-        } flex flex-col items-start duration-300 overflow-hidden justify-start  bg-[#427D9D] w-60 transition-all z-20 `}
+        } flex flex-col items-start duration-300 overflow-hidden justify-start  bg-[#427D9D] w-60 transition-all z-20 relative`}
       >
-        <li className="text-xl text-white border-b-2 border-white  block w-full hover:pl-4 cursor-pointer transition-all duration-300">
+        <li className="text-xl z-20 text-white border-b-2 border-white  block w-full hover:pl-4 cursor-pointer transition-all duration-300">
           <Link className="py-4 block h-full px-4 w-full " href="/tables">
             tables
           </Link>
         </li>
-        <li className="text-xl text-white border-b-2 border-white  block w-full hover:pl-4 cursor-pointer transition-all duration-300">
+        <li className="text-xl z-20 text-white border-b-2 border-white  block w-full hover:pl-4 cursor-pointer transition-all duration-300">
           <Link className="py-4 block h-full px-4 w-full " href="/labs">
             labs
           </Link>
         </li>
-        <li className="text-xl text-white border-b-2 border-white  block w-full hover:pl-4 cursor-pointer transition-all duration-300">
+        <li className="text-xl z-20 text-white border-b-2 border-white  block w-full hover:pl-4 cursor-pointer transition-all duration-300">
           <Link className="py-4 block h-full px-4 w-full " href="/projects">
             projects
           </Link>
         </li>
-        <li className="text-xl text-white border-b-2 border-white  block w-full hover:pl-4 cursor-pointer transition-all duration-300">
+        <li className="text-xl z-20 text-white border-b-2 border-white  block w-full hover:pl-4 cursor-pointer transition-all duration-300">
           <Link className="py-4 block h-full px-4 w-full " href="/subjects">
             subjects
           </Link>
         </li>
-        <li className="text-xl text-white   block w-full hover:pl-4 cursor-pointer transition-all duration-300">
+        <li className="text-xl z-20 text-white   block w-full hover:pl-4 cursor-pointer transition-all duration-300">
           <Link className="py-4 block h-full px-4 w-full " href="/drives">
             drives
           </Link>
